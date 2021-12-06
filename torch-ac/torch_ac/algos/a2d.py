ab5a7eb2a2109766c76156611f01d3740fc6ba33
sumitrc9 <sumit.rc9@gmail.com>
2021-12-06 15:57:53 -0500

    sus a2d

--- torch-ac/torch_ac/algos/a2d.py
+++ torch-ac/torch_ac/algos/a2d.py
@@ -26,7 +26,10 @@ class A2DAlgo(BaseAlgo):
 
             expert_obs.append(expert_ob)
 
+        shape = (self.num_frames_per_proc, self.num_procs)
+
         self.expert_obs = expert_obs
+        self.expert_obss = [None]*(shape[0])
 
         self.expert_model = expert_model
         self.beta = 1
@@ -48,25 +51,32 @@ class A2DAlgo(BaseAlgo):
                 e = random.uniform(0, 1)
 
                 # Expert query at random
-                if e < self.beta:
-                    preprocessed_obs = self.preprocess_obss(self.expert_obs, device=self.device)
-                    if self.expert_model.recurrent:
-                        dist, value, memory = self.expert_model(preprocessed_obs, self.memory * self.mask.unsqueeze(1))
-                    else:
-                        dist, value = self.expert_model(preprocessed_obs)
+                
+                preprocessed_obs = self.preprocess_obss(self.expert_obs, device=self.device)
+                if self.expert_model.recurrent:
+                    dist, value, memory = self.expert_model(preprocessed_obs, self.memory * self.mask.unsqueeze(1))
                 else:
-                    preprocessed_obs = self.preprocess_obss(self.obs, device=self.device)
-                    if self.expert_model.recurrent:
-                        dist, value, memory = self.acmodel(preprocessed_obs, self.memory * self.mask.unsqueeze(1))
-                    else:
-                        dist, value = self.acmodel(preprocessed_obs)
+                    dist, value = self.expert_model(preprocessed_obs)
+                
+                preprocessed_obs_ac = self.preprocess_obss(self.obs, device=self.device)
+                if self.expert_model.recurrent:
+                    dist_learner, value_learner, memory_learner = self.acmodel(preprocessed_obs_ac, self.memory * self.mask.unsqueeze(1))
+                else:
+                    dist_learner, value_learner = self.acmodel(preprocessed_obs_ac)
         
+            dist = self.beta * dist + (1 - self.beta) * dist_learner
+            value = self.beta * value + (1 - self.beta) * value_learner
+            
+            if self.expert_model.recurrent:
+                memory = self.beta * memory + (1 - self.beta) * memory_learner
+
             self.update_beta()
             action = dist.sample()
 
             obs, reward, done, _ = self.env.step(action.cpu().numpy())
 
             self.obss[i] = self.obs
+            self.expert_obss[i] = self.expert_obs
 
             import copy 
             expert_obs = list()
@@ -116,6 +126,25 @@ class A2DAlgo(BaseAlgo):
             self.log_episode_reshaped_return *= self.mask
             self.log_episode_num_frames *= self.mask
 
+        # Add advantage and return to experiences
+
+        preprocessed_obs = self.preprocess_obss(self.obs, device=self.device)
+        with torch.no_grad():
+            if self.acmodel.recurrent:
+                _, next_value, _ = self.acmodel(preprocessed_obs, self.memory * self.mask.unsqueeze(1))
+            else:
+                _, next_value = self.acmodel(preprocessed_obs)
+
+        for i in reversed(range(self.num_frames_per_proc)):
+            next_mask = self.masks[i+1] if i < self.num_frames_per_proc - 1 else self.mask
+            next_value = self.values[i+1] if i < self.num_frames_per_proc - 1 else next_value
+            next_advantage = self.advantages[i+1] if i < self.num_frames_per_proc - 1 else 0
+
+            delta = self.rewards[i] + self.discount * next_value * next_mask - self.values[i]
+            self.advantages[i] = delta + self.discount * self.gae_lambda * next_advantage * next_mask
+        
+        #advantages, returns = self.advantage_and_return(self.acmodel.parameters(), sb.reward, value, sb.mask)
+
         # Define experiences:
         #   the whole experience is the concatenation of the experience
         #   of each process.
@@ -128,6 +157,9 @@ class A2DAlgo(BaseAlgo):
         exps.obs = [self.obss[i][j]
                     for j in range(self.num_procs)
                     for i in range(self.num_frames_per_proc)]
+        exps.expert_obs = [self.expert_obss[i][j]
+                    for j in range(self.num_procs)
+                    for i in range(self.num_frames_per_proc)]
         if self.acmodel.recurrent:
             # T x P x D -> P x T x D -> (P * T) x D
             exps.memory = self.memories.transpose(0, 1).reshape(-1, *self.memories.shape[2:])
@@ -137,12 +169,15 @@ class A2DAlgo(BaseAlgo):
         exps.action = self.actions.transpose(0, 1).reshape(-1)
         exps.value = self.values.transpose(0, 1).reshape(-1)
         exps.reward = self.rewards.transpose(0, 1).reshape(-1)
+        exps.advantage = self.advantages.transpose(0, 1).reshape(-1)
         exps.returnn = exps.value
         exps.log_prob = self.log_probs.transpose(0, 1).reshape(-1)
 
         # Preprocess experiences
 
         exps.obs = self.preprocess_obss(exps.obs, device=self.device)
+        exps.expert_obs = self.preprocess_obss(exps.expert_obs, device=self.device)
+
 
         # Log some values
 
@@ -190,20 +225,38 @@ class A2DAlgo(BaseAlgo):
 
             # Compute loss
 
-            if self.acmodel.recurrent:
-                dist, value, memory = self.acmodel(sb.obs, memory * sb.mask)
+
+            if self.expert_model.recurrent:
+                dist, value, memory = self.expert_model(sb.expert_obs, self.memory * self.mask.unsqueeze(1))
             else:
-                dist, value = self.acmodel(sb.obs)
+                dist, value = self.expert_model(sb.expert_obs)
+            
+            if self.expert_model.recurrent:
+                dist_learner, value_learner, memory_learner = self.acmodel(sb.obs, self.memory * self.mask.unsqueeze(1))
+            else:
+                dist_learner, value_learner = self.acmodel(sb.obs)
+        
+            dist_weighted = self.beta * dist + (1 - self.beta) * dist_learner
+            value = self.beta * value + (1 - self.beta) * value_learner
+            if self.expert_model.recurrent:
+                memory = self.beta * memory + (1 - self.beta) * memory_learner
+
 
             #A2D
+             # Take value function steps.
+            # value_loss = value_step(self, inputs, returns, advantages, not_dones, fn_val, fn_val_opt)
 
-            advantages, returns = self.advantage_and_return(self.acmodel.parameters(), sb.reward, value, sb.mask)
+            # # Take policy steps.  Replaced unused args with None.
+            # if policy_step:
+            #         # Do the importance weight.
+            #         # advantages *= importance_weight(A2D_class, policy_tag, states, obs, actions)
+            #     # Take the RL step.
+            #     trpo_step(inputs, actions, action_log_probs, None, None, None, advantages, fn_pol, A2D_class.params, None)
             
+            entropy = dist_weighted.entropy().mean()
+
+            policy_loss = -(( dist / dist_weighted) * sb.advantage).mean()
             
-            entropy = dist.entropy().mean()
-
-            policy_loss = -(dist.log_prob(sb.action)).mean()
-
             value_loss = (value - sb.returnn).pow(2).mean()
 
             loss = policy_loss - self.entropy_coef * entropy + self.value_loss_coef * value_loss
@@ -262,39 +315,23 @@ class A2DAlgo(BaseAlgo):
         starting_indexes = numpy.arange(0, self.num_frames, self.recurrence)
         return starting_indexes
 
-    def advantage_and_return(params, rewards, values, not_dones):
-        V_s_tp1 = torch.cat([values[:, 1:], values[:, -1:]], 1) * (not_dones == 1)
-        deltas = rewards + params.gamma * V_s_tp1 - values
 
-        advantages = torch.zeros_like(rewards)
-        returns = torch.zeros_like(rewards)
-        indices = get_path_indices(not_dones)
-        for agent, start, end in indices:
-            advantages[agent, start:end] = discount_path(deltas[agent, start:end], params.lambda_ * params.gamma)
-            returns[agent, start:end] = discount_path(rewards[agent, start:end], params.gamma)
+def kl_loss(learner_log_dist, expert_log_dist):
+    """
+    AW - Compute KL divergence.
+    :param learner_log_dist:    (tensor):     log probability distribution over actions from learner.
+    :param expert_log_dist:     (tensor):     log probability distribution over actions from expert.
+    :return:
+    """
 
-        return advantages.clone().detach(), returns.clone().detach()
+    # Check type.
+    if type(learner_log_dist) == tuple:
+        learner_log_dist = learner_log_dist[0]
+    if type(expert_log_dist) == tuple:
+        expert_log_dist = expert_log_dist[0]
 
-def discount_path(path, h):
-        curr = 0
-        rets = []
-        for i in range(len(path)):
-            curr = curr*h + path[-1-i]
-            rets.append(curr)
-        rets =  torch.stack(list(reversed(rets)), 0)
-        return rets
-    
-def get_path_indices(not_dones):
-    indices = []
-    num_timesteps = not_dones.shape[1]
-    for actor in range(not_dones.shape[0]):
-        last_index = 0
-        for i in range(num_timesteps):
-            if not_dones[actor, i] == 0.:
-                indices.append((actor, last_index, i + 1))
-                last_index = i + 1
-        if last_index != num_timesteps:
-            indices.append((actor, last_index, num_timesteps))
-    return indices
+    # KL(p||q).
+    loss = torch.sum(torch.softmax(expert_log_dist, dim=-1) * (expert_log_dist - learner_log_dist), dim=1)
+    return loss.mean()
     
     
